13466e5268a04af7ea8d1f897c3b85177ce58e5a
KhaPN <pham.ngoc.kha@sun-asterisk.com>
2026-02-02 08:30:44 +0700

    feat: remove out of date package

--- app/Share/Http/Middleware/SetLocaleMiddleware.php
+++ app/Share/Http/Middleware/SetLocaleMiddleware.php
@@ -4,7 +4,6 @@ namespace App\Share\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
-use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
 use Symfony\Component\HttpFoundation\Response;
 
 class SetLocaleMiddleware
@@ -12,10 +11,6 @@ class SetLocaleMiddleware
     public function handle(Request $request, Closure $next): Response
     {
         $locale = $request->header('x-locale') ?? config('app.locale');
-
-        if (! LaravelLocalization::checkLocaleInSupportedLocales($locale)) {
-            throw new \Exception('Locale not supported: '.$locale);
-        }
 
         app()->setLocale($locale);
 
